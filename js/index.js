8825cae236e719e3e3cc91ddc96789aaa72ba296
lavash2008 <p-bayandin03@mail.ru>
2023-09-02 13:08:07 +0500

    modified:   beauty-compass v4/css/style.css 	deleted:    beauty-compass v4/css/style.css.map 	new file:   chatTech.html 	new file:   css/modules/chats.sass 	modified:   css/modules/index.css 	modified:   css/modules/index.css.map 	modified:   css/modules/index.sass 	new file:   css/modules/pagin.sass 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   css/style.sass 	new file:   img/notification-bing.svg 	new file:   img/unlockGrey.svg 	modified:   index.html 	new file:   js/chats.js 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
             $(element).css('display', 'block')
         }
     }) 
+
+    $('#techChat').on('click',function(){
+        location.replace('chatTech.html');
+    })
+
+    $('#saveProfile').on('click', function(){
+        const data=new FormData($('.persData')[0]);
+        file=data.get('form__pic')
+        if (file) {
+            console.log("Загруженный файл:", file.name);
+        }
+    })
 })
