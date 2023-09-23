1cf3e18289ca93243c21fff65e8705e6c96ea2a0
lavash2008 <p-bayandin03@mail.ru>
2023-09-23 20:44:08 +0500

    modified:   css/modules/_basic.sass 	new file:   css/modules/details.css 	new file:   css/modules/details.sass 	modified:   css/modules/manufacturer.sass 	modified:   css/modules/pagin.sass 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   css/style.sass 	modified:   img/btn_pen.svg 	modified:   js/profile.js 	modified:   lk-users/chat-persons.html 	modified:   lk-users/chat-tech.html 	modified:   lk-users/comments.html 	modified:   manufacturer-lk/profile.html 	modified:   test-pages/saveProgramm.html

--- js/profile.js
+++ js/profile.js
@@ -7,7 +7,7 @@ let isOpen = false;
 
 addInfoTrigger.addEventListener("click", function () {
   if (!isOpen) {
-    addInfoContent.style.maxHeight = addInfoContent.scrollHeight + 20 + "px";
+    addInfoContent.style.maxHeight = "none";
     isOpen = true;
     addInfoArrow.style.transform = "rotate(180deg)";
     addInfoContent.style.margin = "1.5rem 0 1rem 0";
@@ -21,22 +21,22 @@ addInfoTrigger.addEventListener("click", function () {
 });
 
 // tooltip
-tippy(".left__question", {
-  content: '<div class="cloud">Пример текста</div>',
-  theme: "light",
-  placement: "top-start",
-  arrow: false,
-  allowHTML: true,
-});
+// tippy(".left__question", {
+//   content: '<div class="cloud">Пример текста</div>',
+//   theme: "light",
+//   placement: "top-start",
+//   arrow: false,
+//   allowHTML: true,
+// });
 
-tippy(".right__question", {
-  content:
-    '<div class="cloud">Вы можете задать порядок отображения средств перемещая их иконки</div>',
-  theme: "light",
-  placement: "top-start",
-  arrow: false,
-  allowHTML: true,
-});
+// tippy(".right__question", {
+//   content:
+//     '<div class="cloud">Вы можете задать порядок отображения средств перемещая их иконки</div>',
+//   theme: "light",
+//   placement: "top-start",
+//   arrow: false,
+//   allowHTML: true,
+// });
 
 // creating brands
 
@@ -170,3 +170,411 @@ modalPricesCells.forEach((cell) => {
     }
   });
 });
+
+var outputInfoDetails=''
+
+// Сортировка по столбцу "очередь показа"
+// $.ajax({
+//   type: "post",
+//   url: "url",
+//   dataType: "json",
+//   success: function (response) {
+//     outputInfoDetails=response
+//   }
+// });
+
+//предпологаемый и желаемый JSON данные
+outputInfoDetails=[{
+  idDB:1,
+  name:"Производитель1",
+  place_display:"1",
+  brands:[
+    {
+      idDB:1,
+      name:"Бранд1",
+      place_display:"1",
+      series:[
+        {
+          idDB:1,
+          name:"Серия1",
+          place_display:"1",
+          cosmetics:[
+            {
+              idDB:1,
+              name:"Средство1",
+              place_display:"1",
+            },
+            {
+              idDB:2,
+              name:"Средство2",
+              place_display:"2",
+            },
+            {
+              idDB:3,
+              name:"Средство3",
+              place_display:"3",
+            },
+            {
+              idDB:4,
+              name:"Средство4",
+              place_display:"4",
+            },
+          ]
+        },
+        {
+          idDB:2,
+          name:"Серия2",
+          place_display:"2",
+          cosmetics:[
+            {
+              idDB:1,
+              name:"Средство1",
+              place_display:"1",
+            },
+            {
+              idDB:2,
+              name:"Средство2",
+              place_display:"2",
+            },
+            {
+              idDB:3,
+              name:"Средство3",
+              place_display:"3",
+            },
+            {
+              idDB:4,
+              name:"Средство4",
+              place_display:"4",
+            },
+          ]
+        },
+      ]
+    },
+    {
+      idDB:2,
+      name:"Бранд2",
+      place_display:"2",
+      cosmetics:[
+        {
+          idDB:1,
+          name:"Средство1",
+          place_display:"1",
+        },
+        {
+          idDB:2,
+          name:"Средство2",
+          place_display:"2",
+        },
+        {
+          idDB:3,
+          name:"Средство3",
+          place_display:"3",
+        },
+        {
+          idDB:4,
+          name:"Средство4",
+          place_display:"4",
+        },
+      ]
+    },
+  ]
+}]
+
+
+//функция вывода данных
+function createPagin(valSearch,arrayIdParent ,blockOutput, _data, _classEl,_lastItem=false,  _placeholderSearch='', _textButtonAdd='',_visSearch=true, _pageSize=5, _pageNumber=1, _pageRange=0){
+  blockOutput.pagination({
+    dataSource: _data,
+    pageSize: 5,
+    pageNumber: 1,
+    pageRange: 0,
+    callback: function(data, pagination) {
+      var html = templatingItem(valSearch,arrayIdParent, data, _classEl, _placeholderSearch, _textButtonAdd, _visSearch, _lastItem);
+      blockOutput.prev().html(html);
+    }
+  })
+}
+
+//шаблон вывода инфы
+function templatingItem(valSearch,arrayIdParent, data, classEl='', placeholderSearch, textButtonAdd, visSearch, lastItem) {
+  
+  html = '<ul>'
+
+  for (let i = 0; i < data.length; i++) {
+    const manufacter = data[i];
+    if(i==0 && visSearch){
+      html+='<div class="input input--general d-flex justify-content-between">'
+      +'<label for="search_'+classEl
+      arrayIdParent.forEach(id=>{
+        html+='_'+id
+      })
+      html+='" class="details__search label label--light label--profile">'
+      +placeholderSearch
+      +'</label>'
+      +'<input  id="searchIn_'+classEl
+      arrayIdParent.forEach(id=>{
+        html+='_'+id
+      })
+      html+='" value="'+valSearch+'" type="text" class="details__search-input '
+      if(lastItem) html+='details__search-input-last'
+      html+='">'
+      +'<a href="" class="details__btn btn btn--blue"> <p class="text--15-25">Добавить '
+      +textButtonAdd
+      +'</p></a>'
+      +'</div>'
+    }
+    html+='<div class="details__view">'
+    +'<input type="text" value="'+manufacter.place_display+'" name="" class="details__view-place" id="">'
+
+    html+='<details class="details">'
+    +'<summary class="details__summary '
+    if(lastItem) html+='details__summary--last-item'
+    html+='">'
+    +'  <p class="title--h5">'
+    +manufacter.name
+    +'  </p> '
+    +'  <div class="details-button">'
+    +'    <a class="details-button__edit btn" href="#">'
+    +'      <img src="../img/btn_pen.svg" alt="">'
+    +'    </a>'
+    +'    <a class="details-button__del btn" href="#">'
+    +'      <img src="../img/destr.svg" alt="">'
+    +'    </a>'
+    +'  </div>'
+    +'</summary>'
+    +'<div class="content">'
+    +'<div class="pagin__output-info">'
+    +'  <p></p>'
+    +'  <div class="pagin__output-info" id="'
+    +classEl+'_'+manufacter.idDB+'_view-all'
+    +'">'
+    +'  </div>'
+    +'</div>'
+    +'</div>'
+    +'</details>'
+    +'</div>'
+  }
+  html += '</ul>'
+
+  return html;
+}
+
+//анимация details
+class Accordion {
+  constructor(el) {
+    this.el = el;
+    this.summary = el.querySelector('summary');
+    this.content = el.querySelector('.content');
+
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener('click', (e) => this.onClick(e));
+  }
+
+  onClick(e) {
+    e.preventDefault();
+    this.el.style.overflow = 'hidden';
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
+    }
+  }
+
+  shrink() {
+    this.isClosing = true;
+    
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => this.isClosing = false;
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => this.isExpanding = false;
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = '';
+  }
+}
+// Присваивание анимаций
+document.querySelectorAll('.add-info__schemes details').forEach((el) => {
+  new Accordion(el);
+});
+
+//вывод информации с помощью пагинации
+//производитель
+let global_club='manuf'
+createPagin('',[0],$('#view_all_info_manufacter'),outputInfoDetails, global_club,false, null, null, false,)
+
+var object=$('#'+global_club+'_'+outputInfoDetails[0].idDB+'_view-all')
+//бренды
+global_club='brand'
+createPagin('',[0],object,outputInfoDetails[0].brands, global_club,false, 'Поиск бренда', 'бренд')
+
+for (let i = 0; i < outputInfoDetails[0].brands.length; i++) {
+  const brand = outputInfoDetails[0].brands[i];
+  
+  object=$('#'+global_club+'_'+brand.idDB+'_view-all')
+  
+  //зависимости есть ли серия у бренда
+  if(brand.series != undefined){
+    createPagin('',[0, i],object, brand.series, 'seria',false, 'Поиск серии', 'серия')
+
+    for (let j = 0; j < brand.series.length; j++) {
+      const seria = brand.series[j];
+      object=$('#seria_'+seria.idDB+'_view-all')
+
+      createPagin('',[0, i, j],object, seria.cosmetics, 'cosmetic',true, 'Поиск средства', 'средство')
+    }
+
+  }else{
+    createPagin('',[0, i],object, brand.cosmetics, 'cosmetic',true, 'Поиск средства', 'средство')
+  }
+}
+
+$('input[id^="searchIn"]').keyup(searching);
+
+function searching(element) { 
+  //вывод новой пагинации по поиску
+  var outputHtml=$(element.currentTarget.parentElement.parentElement.parentElement.parentElement.lastChild)
+  //строка поиска
+  var stroke_seach=element.target.value
+
+  let array_el=element.target.attributes.id.value.split('_');
+
+  let idForFocus=element.target.attributes.id;
+  let classEl=array_el[1]
+  //id_elem [id производителя, id брендов, id серии] в json
+  let id_elem=$.grep(array_el, function(el){
+    return !isNaN(el)
+  })
+
+  var arrayOutput=[];
+  switch (id_elem.length) {
+    case 1:
+        arraySearch=outputInfoDetails[id_elem[0]].brands
+
+        arraySearch.forEach(el=>{
+          if(el.name.includes(stroke_seach))
+            arrayOutput.push(el)
+        })
+        createPagin(stroke_seach,id_elem, outputHtml, arrayOutput, classEl, false, 'Поиск бренда', 'бренд')
+
+        for (let i = 0; i < arrayOutput.length; i++) {
+          const brand = arrayOutput[i];
+          
+          object=$('#'+global_club+'_'+brand.idDB+'_view-all')
+          
+          //зависимости есть ли серия у бренда
+          if(brand.series != undefined){
+            createPagin('',[0, i],object, brand.series, 'seria',false, 'Поиск серии', 'серия')
+        
+            for (let j = 0; j < brand.series.length; j++) {
+              const seria = brand.series[j];
+              object=$('#seria_'+seria.idDB+'_view-all')
+        
+              createPagin('',[0, i, j],object, seria.cosmetics, 'cosmetic',true, 'Поиск средства', 'средство')
+            }
+        
+          }else{
+            createPagin('',[0, i],object, brand.cosmetics, 'cosmetic',true, 'Поиск средства', 'средство')
+          }
+        }
+
+        $('input[id^="searchIn"]').keyup(searching);
+      break;
+    case 2:
+      let triger
+      let placeholder
+      let addText
+
+      if (outputInfoDetails[id_elem[0]].brands[id_elem[1]].series == undefined) {
+        arraySearch=outputInfoDetails[id_elem[0]].brands[id_elem[1]].cosmetics
+        triger=true
+        placeholder='Поиск средства'
+        addText='средство'
+
+        arraySearch.forEach(el=>{
+          if(el.name.includes(stroke_seach))
+            arrayOutput.push(el)
+        })
+  
+        createPagin(stroke_seach,id_elem, outputHtml, arrayOutput, classEl, triger, placeholder,addText)
+      }
+      else {
+        arraySearch=outputInfoDetails[id_elem[0]].brands[id_elem[1]].series
+        triger=true
+        placeholder='Поиск серии'
+        addText='серию'
+
+        arraySearch.forEach(el=>{
+          if(el.name.includes(stroke_seach))
+            arrayOutput.push(el)
+        })
+  
+        createPagin(stroke_seach,id_elem, outputHtml, arrayOutput, classEl, triger, placeholder,addText)
+
+        for (let j = 0; j < arrayOutput.length; j++) {
+          const seria = arrayOutput[j];
+          object=$('#seria_'+seria.idDB+'_view-all')
+    
+          createPagin('',[0, id_elem[1], j],object, seria.cosmetics, 'cosmetic',true, 'Поиск средства', 'средство')
+        }
+      }
+
+      
+      $('input[id^="searchIn"]').keyup(searching);
+      break;
+    case 3:
+        arraySearch=outputInfoDetails[id_elem[0]].brands[id_elem[1]].series[id_elem[2]].cosmetics
+        
+        arraySearch.forEach(el=>{
+          if(el.name.includes(stroke_seach))
+            arrayOutput.push(el)
+        })
+        createPagin(stroke_seach,id_elem, outputHtml, arrayOutput, classEl, true, 'Поиск серии', 'серию')
+      $('input[id^="searchIn"]').keyup(searching);
+      break;
+  }
+
+  $(idForFocus).focus();
+}
